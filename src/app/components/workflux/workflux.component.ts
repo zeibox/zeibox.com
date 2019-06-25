64e965953d7fc6cac6f1ec551c7cbd488202627d
leanpv <lean.puebla@gmail.com>
2019-06-25 18:35:50 -0300

    Creé Workflow Comp, +Imagenes

---
+++ src/app/components/workflux/workflux.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workflux',
+  templateUrl: './workflux.component.html',
+  styleUrls: ['./workflux.component.css']
+})
+export class WorkfluxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
