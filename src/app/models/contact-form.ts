7093910f4d71b6ae90cfea37650c14139d703d2a
Cristian Decoud <cristian@cd.cdecoud.net>
2019-06-27 18:01:02 -0300

    Agregue el formulario de contacto, falta el servicio

---
+++ src/app/models/contact-form.ts
@@ -0,0 +1,14 @@
+export interface ContactFormI {
+  fullName: {
+    name: string;
+    lastName: string;
+  };
+  email: string;
+  interest: string;
+  textarea: string;
+  active: boolean;
+  readed: boolean;
+  blocked: boolean;
+}
+
+
