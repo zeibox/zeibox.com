23c683f2003a4018b377fb3bc56ade81705afd12
Leanpv <lean.puebla@gmail.com>
2019-06-29 21:41:43 -0300

    centré los logos, agregué logos en navChico y footer

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -17,18 +17,21 @@ export class NavbarComponent implements OnInit {
       const navt = document.querySelector('#toggle');
       const navti = document.querySelector('#toggleIcon');
       const navtext = document.querySelector('#navText');
+      const navZei = document.querySelector('#navZeibox');
       if ( window.scrollY <= 10 ) {
         nav.className = 'navbar navbarT fixed-top';
         nav2.className = 'navbar navbarT fixed-top';
         navt.className = 'navbar-toggler white-border';
         navti.className = 'navbar-toggler-icon white-icon';
         navtext.className = 'navbar-brand white';
+        navZei.className = 'navbar-brand white mr-auto zeiboxH';
       } else {
         nav.className = 'navbar fixed-top scroll shadow';
         nav2.className = 'navbar fixed-top scroll shadow';
         navt.className = 'navbar-toggler red-border';
         navti.className = 'navbar-toggler-icon red-icon';
         navtext.className = 'navbar-brand red';
+        navZei.className = 'navbar-brand red mr-auto zeiboxH';
       }
     };
   }
@@ -39,6 +42,7 @@ export class NavbarComponent implements OnInit {
     const navt = document.querySelector('#toggle');
     const navti = document.querySelector('#toggleIcon');
     const navtext = document.querySelector('#navText');
+    const navZei = document.querySelector('#navZeibox');
     if( this.opciones === 'none') {
       this.opciones = 'inline';
       if ( window.scrollY <= 10 ) {
@@ -47,12 +51,14 @@ export class NavbarComponent implements OnInit {
         navt.className = 'navbar-toggler white-border';
         navti.className = 'navbar-toggler-icon white-icon';
         navtext.className = 'navbar-brand white';
+        navZei.className = 'navbar-brand white mr-auto zeiboxH';
       } else {
         nav.className = 'navbar fixed-top scroll shadow';
         nav2.className = 'navbar fixed-top scroll shadow';
         navt.className = 'navbar-toggler red-border';
         navti.className = 'navbar-toggler-icon red-icon';
         navtext.className = 'navbar-brand red';
+        navZei.className = 'navbar-brand red mr-auto zeiboxH';
       }
     } else {
       this.opciones = 'none';
@@ -62,12 +68,14 @@ export class NavbarComponent implements OnInit {
         navt.className = 'navbar-toggler white-border';
         navti.className = 'navbar-toggler-icon white-icon';
         navtext.className = 'navbar-brand white';
+        navZei.className = 'navbar-brand white mr-auto zeiboxH';
       } else {
         nav.className = 'navbar fixed-top scroll shadow';
         nav2.className = 'navbar fixed-top scroll shadow';
         navt.className = 'navbar-toggler red-border';
         navti.className = 'navbar-toggler-icon red-icon';
         navtext.className = 'navbar-brand red';
+        navZei.className = 'navbar-brand red mr-auto zeiboxH';
       }
     }
   }
