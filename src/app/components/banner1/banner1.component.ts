c05025465b89ae798a81ded8678fe017b0006b0b
Cristian Decoud <cristian@cd.cdecoud.net>
2019-08-12 16:23:01 -0300

    Agregue animacion en el titulo

--- src/app/components/banner1/banner1.component.ts
+++ src/app/components/banner1/banner1.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { WOW } from 'wowjs/dist/wow.min';
 @Component({
   selector: 'app-banner1',
   templateUrl: './banner1.component.html',
   styleUrls: ['./banner1.component.css']
 })
-export class Banner1Component implements OnInit {
+export class Banner1Component implements AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    new WOW().init();
+  }
 }
